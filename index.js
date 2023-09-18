f3379daae88e344f3ef278823f48863c0554ace9
RaikkoP <RaikkoP@users.noreply.github.com>
2023-09-18 16:27:44 +0300

    Testing and stuff :(

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 //Packages
 const express = require('express');
 const mysql = require('mysql');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
+const hbs = require('express-handlebars');
 require('dotenv').config();
+
+//body parser
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use('cors');
 
 //Db Connection
 
@@ -16,6 +24,14 @@ const db = mysql.createConnection({
 app.get('/', (req, res) => {
     console.log("Connected")
 });
+
+app.post('/test', (req, res) => {
+    if(process.env.USER_REGEX.test(req.body.username)){
+        console.log("Poggers")
+    } else {
+        console.log("Fuck you")
+    }
+}); 
 
 
 app.listen(3000, () => {
