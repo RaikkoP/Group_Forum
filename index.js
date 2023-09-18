f7cc5a502afeaad58f05a4b510c1cdd30dcf68ac
RaikkoP <RaikkoP@users.noreply.github.com>
2023-09-18 15:37:37 +0300

    Fixed env errors

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ require('dotenv').config();
 //Db Connection
 
 const db = mysql.createConnection({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_DATABASE,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 });
 
 app.get('/', (req, res) => {
