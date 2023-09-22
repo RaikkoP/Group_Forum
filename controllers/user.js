edcb9435dd051dec00492361685b67cbb3486656
RaikkoP <RaikkoP@users.noreply.github.com>
2023-09-22 12:45:24 +0300

    Added registration, routes, controller, model

---
+++ controllers/user.js
@@ -0,0 +1,23 @@
+const User = require("../models/users.model");
+
+
+module.exports = {
+    registerUser: async (req, res) => {
+        const userData = new User({
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email
+        })
+    
+        User.registerNew(userData, (err, data) => {
+            if (err) {
+                res.status(500).send({
+                    message: err.message || "Error making new account"
+                })
+            } else {
+                req.session.authenticated = true;
+                console.log(data);
+            }
+        })
+    }
+}
