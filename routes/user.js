0f0b2c8a30e94ceeb6854ccda479ac4aa30758bc
RaikkoP <raikko.prants@gmail.com>
2023-09-29 16:40:13 +0300

    Edited routers

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user');
 
-router.post('/', userController.registerUser);
+router.post('/register', userController.registerUser);
 router.get('/register',userController.showRegistration)
 router.get('/login',userController.showLogin)
 router.get('/home',userController.getForum)
