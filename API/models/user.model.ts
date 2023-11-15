6827aa527c083d318ddc6a20f9513bb350f5da86
RaikkoP <raikko.prants@gmail.com>
2023-11-15 11:54:57 +0200

    Made backend more redable and fixed a few issues

--- API/models/user.model.ts
+++ API/models/user.model.ts
@@ -1,6 +1,6 @@
 import { MysqlError } from "mysql";
 import db from "../utility/database";
-import regex from "../utility/regex_configuration";
+import passRegex from "../utility/regex_check";
 import bcrypt = require("bcryptjs");
 
 interface UserInterface {
@@ -25,70 +25,49 @@ class User {
     this.email = email;
   }
 
-  static login(
-    user: UserInterface,
-    result: (
-      error: ErrorInterface | string | null,
-      data: UserInterface | null
-    ) => void
-  ) {
+  static login(user: UserInterface, result: (error: ErrorInterface | string | null,data: UserInterface | null) => void) {
     //Check inputs with regex
-    if (
-      !regex.userRegEx.test(user.username) &&
-      !regex.passwordRegEx.test(user.password)
-    ) {
-      result("Failed to login", null);
+    if (!passRegex(user.username, user.password)) {
+      result("Failed to login", null)
       return;
     }
-
     //See if user with this username exists
-    let queryFindUser = `SELECT * FROM users WHERE
-  username = ?`;
-    //SQL COMMAND
-    db.query(queryFindUser, [user.username], async (err, res) => {
-      if (res.length == 0) {
+    db.query('SELECT * FROM users WHERE username = ?', [user.username], async (err, res) => {
+      if (res.length == 0 || res == null || undefined) {
         result(err, res);
         return;
       }
-      if (
-        res.length > 0 &&
-        (await bcrypt.compare(user.password, res[0].password))
-      ) {
+      await bcrypt.compare(user.password, res[0].password)
+       {
         if (err) {
           result(err, res);
           return;
         }
         result(null, { id: res.insertId, ...user });
-      }
-    });
+      }});
   }
 
   static register(user: UserInterface, result: (error: MysqlError | string | null, data: UserInterface | null) => void) {
     //Check inputs with regex
-    if (
-      !regex.userRegEx.test(user.username) &&
-      !regex.passwordRegEx.test(user.password)
-    ) {
-      result("Nope haha", null);
+    if (!passRegex(user.username, user.password)) {
+      result("Failed to login", null)
       return;
     }
-    //Check if username or email exists in db
-    let queryCheckUsername = `SELECT username FROM users
-WHERE username = ? or email = ?`;
-    //Add user to database if all is secure and well, save password as HASH NOT NORMAL PASSWORD
-    let queryAddUser = `INSERT INTO users SET
-username = '?',
-password = ?,
-email = ?`;
     //Username check
-    db.query(queryCheckUsername, [user.username, user.email], (err, res) => {
+    //Check if username or email exists in db
+    db.query('SELECT username FROM users WHERE username = ? or email = ?', [user.username, user.email], (err, res) => {
       console.log(res);
       if (res.length > 0) {
         console.log("That username or email is taken");
         result(err, null);
         return;
       }
-      db.query(queryAddUser, [user.username, bcrypt.hashSync(user.password, bcrypt.genSaltSync(10)), user.email], (err, res) => {
+      if (res == null || undefined) {
+        result(err, null);
+        return;
+      }
+      //Add user to database if all is secure and well, save password as HASH NOT NORMAL PASSWORD
+      db.query('INSERT INTO users SET username = ?, password = ?, email = ?', [user.username, bcrypt.hashSync(user.password, bcrypt.genSaltSync(10)), user.email], (err, res) => {
         if (err) {
           console.log("error: ", err);
           result(err, null);
